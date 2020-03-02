b8a785e5969483df034a9424f97e63042d85d15d
Guo Zongyi <53574662+GZYNus@users.noreply.github.com>
2020-03-02 13:14:45 +0800

    Update hord_search_wrapper_10_folds.py

--- Predicting Alzheimer's Disease Using ML/HORD_search_wrapper/hord_search_wrapper_10_folds.py
+++ Predicting Alzheimer's Disease Using ML/HORD_search_wrapper/hord_search_wrapper_10_folds.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 """
 Description:
-Date: 11/7/19
-Email: anlijuncn@gmail.com
-Writen by Lijun AN and CBIG under MIT license:
-https://github.com/ThomasYeoLab/CBIG/blob/master/LICENSE.md
+Email:gzynus@gmail.com  
+Arthur: Zongyi Guo
 """
 import os
 import argparse
