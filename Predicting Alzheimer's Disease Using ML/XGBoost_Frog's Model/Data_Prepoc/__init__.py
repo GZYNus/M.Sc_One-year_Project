af617ce4b7d44caeeb6f4251ab2252d76b4656e0
Guo Zongyi <53574662+GZYNus@users.noreply.github.com>
2020-03-02 12:05:20 +0800

    Delete __init__.py

--- Predicting Alzheimer's Disease Using ML/XGBoost_Frog's Model/Data_Prepoc/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""
-Description: 
-Date: 13/11/19 
-Email: anlijuncn@gmail.com
-Writen by Lijun AN and CBIG under MIT license:
-https://github.com/ThomasYeoLab/CBIG/blob/master/LICENSE.md
-"""
