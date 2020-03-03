6c4280c13bb3f72cb70778a04af32845b7fe177c
Guo Zongyi <53574662+GZYNus@users.noreply.github.com>
2020-03-04 01:12:38 +0800

    Delete __init__.py

--- Predicting Alzheimer's Disease Using ML/Minimal_RNN Model/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""
-Description: model
-Date: 7/7/19 
-Email: anlijuncn@gmail.com
-Writen by Lijun AN and CBIG under MIT license:
-https://github.com/ThomasYeoLab/CBIG/blob/master/LICENSE.md
-"""
